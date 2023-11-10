ff7727d75a0441bf87d48f1264f40558b05f8899
Patrick Nance <jpnance@gmail.com>
2023-11-10 12:35:26 -0800

    Refactoring to remove some obvious duplication.

--- server.mjs
+++ server.mjs
@@ -3,14 +3,22 @@ import express from 'express';
 const server = express();
 const port = process.env.PORT;
 
+const players = [];
+
 server.get('/', (request, response) => {
-	response.send('<html><head><title>Players</title></head><body><form method="post"><ul></ul><label>Name <input type="text" /></label><button>Add to Player Pool</button></form></body></html>');
+	response.send(`<html><head><title>Players</title></head><body><form method="post"><ul>${players.map(toListItem)}</ul><label>Name <input type="text" /></label><button>Add to Player Pool</button></form></body></html>`);
 });
 
 server.post('/', (request, response) => {
-	response.send('<html><head><title>Players</title></head><body><form method="post"><ul><li>Vince Young</li></ul><label>Name <input type="text" /></label><button>Add to Player Pool</button></form></body></html>');
+	players.push('Vince Young');
+
+	response.redirect('/');
 });
 
 server.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
+
+function toListItem(value) {
+	return `<li>${value}</li>`;
+}
