dc3ef2bd76382a4a84fed260ab6692dd46828d4c
Patrick Nance <jpnance@gmail.com>
2023-11-10 12:27:13 -0800

    Add any player you want as long as it's Vince Young.

--- home.test.js
+++ home.test.js
@@ -5,3 +5,31 @@ test('Has title', async ({ page }) => {
 
 	await expect(page).toHaveTitle(/Players/);
 });
+
+test('Can add player to player pool', async ({ page }) => {
+	await page.goto('http://localhost:3000/');
+
+	const beforeSubmit = {
+		players: page.getByRole('listitem'),
+		nameInput: page.getByRole('textbox', { name: 'Name' }),
+		submitButton: page.getByRole('button', { name: 'Add to Player Pool' })
+	};
+
+	await expect(beforeSubmit.players).toHaveCount(0);
+
+	await expect(beforeSubmit.nameInput).toBeVisible();
+	await expect(beforeSubmit.submitButton).toBeVisible();
+
+	await beforeSubmit.nameInput.fill('Vince Young');
+
+	await beforeSubmit.submitButton.click();
+
+	const afterSubmit = {
+		players: page.getByRole('listitem')
+	};
+
+	afterSubmit.players = page.getByRole('listitem');
+
+	await expect(afterSubmit.players).toHaveCount(1);
+	await expect(afterSubmit.players).toHaveText('Vince Young');
+});
--- server.mjs
+++ server.mjs
@@ -4,7 +4,11 @@ const server = express();
 const port = process.env.PORT;
 
 server.get('/', (request, response) => {
-	response.send('<html><head><title>Players</title></head></html>');
+	response.send('<html><head><title>Players</title></head><body><form method="post"><ul></ul><label>Name <input type="text" /></label><button>Add to Player Pool</button></form></body></html>');
+});
+
+server.post('/', (request, response) => {
+	response.send('<html><head><title>Players</title></head><body><form method="post"><ul><li>Vince Young</li></ul><label>Name <input type="text" /></label><button>Add to Player Pool</button></form></body></html>');
 });
 
 server.listen(port, () => {
