fdf7119e56791a75ef3e2d30d3745c63e852ad96
Patrick Nance <jpnance@gmail.com>
2023-11-10 10:14:53 -0800

    Externalizing the port number we use for this.

--- server.mjs
+++ server.mjs
@@ -1,11 +1,12 @@
 import express from 'express';
 
 const server = express();
+const port = process.env.PORT;
 
 server.get('/', (request, response) => {
 	response.send('<html><head><title>Rostered Players</title></head></html>');
 });
 
-server.listen(3000, () => {
-	console.log('Listening on port 3000');
+server.listen(port, () => {
+	console.log(`Listening on port ${port}`);
 });
